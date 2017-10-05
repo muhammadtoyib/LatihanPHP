db1748bd323902d2c634f3136c200b9ce2ad6af4
muhammadtoyib <32325330+muhammadtoyib@users.noreply.github.com>
2017-10-06 01:48:31 +0700

    Praktek Pert. 2 - Update Matkul

--- Pert 2/prosesmatakuliah.php
+++ Pert 2/prosesmatakuliah.php
@@ -6,6 +6,12 @@ if($_GET['action'] == "add"){
 	//2. Query 
 	$query = "INSERT INTO matakuliah (kode_mtk, nama_mtk)
 			VALUES('$_POST[kode_mtk]', '$_POST[nama_mtk]')";
+}else if($_GET['action'] == 'edit'){
+	//2. Query 
+	$query = = "UPDATE matakuliah 
+				SET kode_dosen = '$_POST[kode_mtk]',
+					nama = '$_POST[nama_mtk]',
+				WHERE id = $_POST[id]";
 }
 
 mysqli_query($koneksi, $query);
