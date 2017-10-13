3ff211c682fa48f66cafbfe0718dac139a400b19
muhammadtoyib <32325330+muhammadtoyib@users.noreply.github.com>
2017-10-13 15:08:32 +0700

    Praktek Pertemuan 3 - CRUD Kategori
    Praktek Pertemuan 3 - CRUD Kategori

---
+++ Pert 3/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+$db = mysqli_connect("localhost", "root", "", "kalbis_web");
+
+if(mysqli_connect_errno()){
+	die(mysqli_connect_error());
+}
+?>
---
+++ Pert 3/prosestambahkategori.php
@@ -0,0 +1,12 @@
+<?php
+
+include("koneksi.php");
+
+$ket = mysqli_real_escape_string ($db, $_POST['ket']);
+$query = "INSERT INTO kategori
+(keterangan) VALUES ('$ket')";
+
+mysqli_query($db, $query);
+
+header('Location: kategori.php');
+?>
