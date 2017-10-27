8babe57134f1d909c96169d27f3b02b88b748ef5
Muhammad Toyib <32325330+muhammadtoyib@users.noreply.github.com>
2017-10-27 09:41:55 +0700

    Pert 3 - Regis, Login & Password Encrypt Phone Book
    Pert 3 - Regis, Login & Password Encrypt Phone Book

---
+++ Pert 3/login.php
@@ -0,0 +1,22 @@
+<h1>LOGIN</h1>
+
+<?php 
+	session_start();
+	if(isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+		header('Location: index.php');
+	}
+?>
+
+<h1> Login </h1>
+
+<form action = "proses_login.php" method = "post">
+
+	Username :
+	<input type="text" name="username" /><br />
+
+	Password :
+	<input type="text" name="password" /><br />
+
+	<input type="submit" name="simpan" />
+
+</form>
